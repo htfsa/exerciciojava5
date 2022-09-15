652e7357906af16053999279896a98915c1e572d
Herbert Ferreira da Silva azevedo <112709078+kakarotofla@users.noreply.github.com>
2022-09-15 16:04:59 -0300

    exerciciojava5

---
+++ build/bundle.js
@@ -0,0 +1,8 @@
+var idade = 27;
+var nome = 'diogo';
+var maiorDeidade = true;
+function soma(num1, num2) {
+    var soma = num1 + num2;
+    return num1 + num2;
+}
+document.write('a soma de 7+5 é ${soma 7,5}');
---
+++ src/script.ts
@@ -0,0 +1,12 @@
+let idade: number = 27;
+const nome : string = 'diogo';
+const maiorDeidade: boolean = true;
+
+   function soma(num1:number, num2: number): number {
+    const soma: number = num1 + num2;
+    return num1 + num2;
+    
+   }
+   document.write('a soma de 7+5 é ${soma 7,5}')
+                
+
